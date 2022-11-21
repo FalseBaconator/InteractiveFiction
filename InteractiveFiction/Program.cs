e8c093fef71ba4ab89f7b1dbb54dc55303c32f38
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-11-21 11:29:31 -0400

    Display story and begin making gameloop

--- InteractiveFiction/Program.cs
+++ InteractiveFiction/Program.cs
@@ -8,8 +8,72 @@ namespace InteractiveFiction
 {
     internal class Program
     {
+
+        static string[] story = new string[]{
+            "Test 1;Test 2;Test 3;1;2",
+            "Test 2",
+            "Test 3"
+        };
+
+        static bool gameOver = false;
+
+        static string[] page;
+        static int currentPage = 0;
+        static int menuPos;
+        static char
+
         static void Main(string[] args)
         {
+            Console.SetWindowSize(49, 13);
+            while(gameOver == false)
+            {
+                WritePage(currentPage);
+
+            }
+            
         }
+
+
+        static void DrawUI()
+        {
+            Console.CursorVisible = false;
+            Console.WriteLine();
+            Console.WriteLine(" +---------------------------------------------+");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(" |                                             |");
+            }
+            Console.WriteLine(" +----------------------+----------------------+");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(" |                      |                      |");
+            }
+            Console.WriteLine(" +----------------------+----------------------+");
+        }
+
+        static void WritePage(int pageNum)
+        {
+            Console.Clear();
+            DrawUI();
+            page = story[pageNum].Split(';');
+            if (page.Length == 5)
+            {
+                Console.SetCursorPosition(3, 3);
+                Console.Write(page[0]);
+                Console.SetCursorPosition(3, 9);
+                Console.Write(page[1]);
+                Console.SetCursorPosition(26, 9);
+                Console.Write(page[2]);
+            }
+            else
+            {
+                Console.SetCursorPosition(2, 2);
+                Console.WriteLine(story[pageNum]);
+                gameOver = true;
+            }
+        }
+
+
+
     }
 }
