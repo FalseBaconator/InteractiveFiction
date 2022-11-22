a15050234ccdce7029b68e8d9fea12deb8975e08
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-11-22 11:06:14 -0400

    started writing story based off of Actual Cannibal Shia LaBeouf

--- InteractiveFiction/Program.cs
+++ InteractiveFiction/Program.cs
@@ -10,10 +10,25 @@ namespace InteractiveFiction
     {
 
         static string[] story = new string[]{
-            "Test 1 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA;Test 2;Test 3;1;2",
-            //"Test 1; Test 2; Test 3;1;2",
-            "Test 2",
-            "Test 3 ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 1234567891011121314151617181920"
+            "You're walking in the woods. there's no one around and your phone is dead. Out of the corner of your eye you spot someone.;Approach;Stay Away;1;0",                    //0
+            "You can now see that the figure is Shia LaBeouf. He gets down on all fours and breaks into a spring, he's gaining on you!;Look for your car;Just run;2;4",             //1
+            "You're looking for your car but you're all turned around. He's almost upon you now and you can see there's blood on his face.;Inspect;Just run;3;4",                   //2
+            "Upon further inspection you can see that the blood is mostly focused around his mouth and hands. He's also holding a knife.;Just run;Disarm him;4;5",                  //3
+            "You're running for your life from Shia LaBeouf. As he gains on you, you realize you can't outrun him.;Zig zag;Duck under branch;6;7",                                  //4
+            "As you reach to disarm him he swings his knife towards you, slashing you across the face. You fall to the ground.",                                                    //5
+            "Now it's dark, and you seem to have lost him, but you're hopelessly lost yourself, creeping silently through the underbrush.;Look around;Follow own footsteps;8;0",    //6
+            "You find a large branch and duck under it with the hope that Shia would duck under it. Sadly he ducks and lunges at you.",                                             //7
+            "Aha! In the distance, a small cottage with a light on! Hope! You move carefully towards it...;Looking for Shia;Looking for danger;9;0",                                //8
+            "You look around above the bushes and see no movement. But your leg, AH! It's caught in a beartrap!;Gnaw leg off;Open beartrap;10;0",                                   //9
+            "You gnaw off your leg as quietly as possible before limping to the cottage. Through the door you see a blood soaked Shia Labeouf.;Run;Hide;0;11",                      //10
+            "As you duck behind the doorway you realize that he hasn't seen you yet. You peek through the door to see him sharpening an axe.;Sneak in;Sneak away;12;0",             //11
+            "You manage to sneak into the cottage and behind Shia LaBeouf",
+
+
+
+
+            "As you go to walk in the other direction you hear a noise behind you. It sounds like the figure is running after you!;Look for your car;Just run;3;4",
+            "The End!"
         };
 
         static bool gameOver = false;
@@ -46,6 +61,8 @@ namespace InteractiveFiction
                     }
                 }
             }
+            Console.ReadKey(true);
+            WritePage(story.Length - 1);
             Console.ReadKey(true);
         }
 
