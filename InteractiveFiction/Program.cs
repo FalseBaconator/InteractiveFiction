335576a9efaa29667dc48b7fc5b81db8fc412e70
Ryan Wohler <ryanwohler55@gmail.com>
2022-11-23 14:55:39 -0400

    Revamped story. Finished one path

--- InteractiveFiction/Program.cs
+++ InteractiveFiction/Program.cs
@@ -10,24 +10,32 @@ namespace InteractiveFiction
     {
 
         static string[] story = new string[]{
-            "You're walking in the woods. there's no one around and your phone is dead. Out of the corner of your eye you spot someone.;Approach;Stay Away;1;0",                    //0
-            "You can now see that the figure is Shia LaBeouf. He gets down on all fours and breaks into a spring, he's gaining on you!;Look for your car;Just run;2;4",             //1
-            "You're looking for your car but you're all turned around. He's almost upon you now and you can see there's blood on his face.;Inspect;Just run;3;4",                   //2
-            "Upon further inspection you can see that the blood is mostly focused around his mouth and hands. He's also holding a knife.;Just run;Disarm him;4;5",                  //3
-            "You're running for your life from Shia LaBeouf. As he gains on you, you realize you can't outrun him.;Zig zag;Duck under branch;6;7",                                  //4
-            "As you reach to disarm him he swings his knife towards you, slashing you across the face. You fall to the ground.",                                                    //5
-            "Now it's dark, and you seem to have lost him, but you're hopelessly lost yourself, creeping silently through the underbrush.;Look around;Follow own footsteps;8;0",    //6
-            "You find a large branch and duck under it with the hope that Shia would duck under it. Sadly he ducks and lunges at you.",                                             //7
-            "Aha! In the distance, a small cottage with a light on! Hope! You move carefully towards it...;Looking for Shia;Looking for danger;9;0",                                //8
-            "You look around above the bushes and see no movement. But your leg, AH! It's caught in a beartrap!;Gnaw leg off;Open beartrap;10;0",                                   //9
-            "You gnaw off your leg as quietly as possible before limping to the cottage. Through the door you see a blood soaked Shia Labeouf.;Run;Hide;0;11",                      //10
-            "As you duck behind the doorway you realize that he hasn't seen you yet. You peek through the door to see him sharpening an axe.;Sneak in;Sneak away;12;0",             //11
-            "You manage to sneak into the cottage and behind Shia LaBeouf",
+            "You awake in a dark and foggy forest. The forest is dense besides a small dirt road before you.;Follow the trail;Try the woods;1;0",                                           //0
+            "You follow the trail to a fork in the road. To your left is the sound of running water, and to your right a faint light.;Go left;Go right;2;0",                                //1
+            "You come across a rushing river. The river seems to have washed up a few dead trees;Swim Across;Make a bridge;3;4",                                                            //2
+            "As you attempt to swim accross, the strong river ends up pulling you under. The last thing you see are the rocks under the river.",                                            //3
+            "You place a dead tree accross the river and cross it. You hear a howling from the woods, much lowder than an ordinary wolf.;Keep to the trail;Hide in the woods;5;0",          //4
+            "You follow the trail, eventually finding an abandonned campsite, with two closed tents and a still burning campfire.;Examine the camp;Move on;6;0",                            //5
+            "The tent on the left is completely empty, but the one on the left has a dead body within it. There's something in it's hand.;Look closer;Leave it alone;7;0",                  //6
+            "Looking closer you see that it's a dagger, if the engraving on the side is to be believed it's made of pure silver;Take it; Leave it alone;8;0",                               //7
+            "The dagger has a bit of weight to it. As you continue down the trail you see another fork in the road. Both paths seem identical.;Go left;Go right;9;0",                       //8
+            "You continue walking down the trail when you hear a branch snap to your left. You only have a moment to prepare to...;Attack;Run;10;0",                                        //9
+            "A monstrous mockery of a wolf jumps out at you when you slash at it with the dagger, cutting it's arm. It falls to the ground.;Examine it;Run;11;12",                          //10
+            "As you get closer to the figure it suddenly lashes out at you. It's claws tear at your ankle as you fall down beside it.",                                                     //11
+            "With the creature injured you take your chance to make a break for it. As you run the thing gets back up and begins to chase you.;Duck into the forest;Attack again;13;14",    //12
+            "You duck into the forest in an attempt to lose the creature. You realize you failed when a claw sinks into your shoulder.",                                                    //13
+            "As you prerpare to attack again you see the figure jump at you. Before you can attack you need to avoid being attacked.;Roll;Duck;15;16",                                      //14
+            "You roll to the side as the creature soars past and falls to the ground. It begins to get back up, hindered by it's injured arm.;Stab it's side;Stab it's head;17;18",         //15
+            "As you duck beneath it's claws, the rest of it simply falls on top of you. It gets off you before, again, you see it's claws.",                                                //16
+            "You stab it in the upper torso below it's arms hoping to cause some damage. It seems to work as the creature stops trying to get up.;Finish it;Just leave;19;20",              //17
+            "You try to stab it in the head, but stabs it's shoulder instead. A sudden hit to your side sends you hurtling into a tree.",                                                   //18
+            "You quickly grab the dagger from it's side and stab it in the neck. With one last swipe it gives you a small cut on the arm.;Follow the trail;Take a breather;21;22",          //19
+            "As you turn to leave the creature uses the last of it's energy to bite your ankle, shattering it. The creature dies finally dies;Follow the trail;Take a breakther;23;22",     //20
+            "You walk down the trail to find your car of all things. You drive to the hospital hoping your small cut doesn't get infected.",                                                //21
+            "You lie against the creature for a quick breather. When you get up to leave you find that the silver dagger now burns your hand.",                                             //22
+            "As you try to walk, your shattered ankle makes you collapse. While you may have defeated the creature, you aren't leaving.",                                                   //23
 
 
-
-
-            "As you go to walk in the other direction you hear a noise behind you. It sounds like the figure is running after you!;Look for your car;Just run;3;4",
             "The End!"
         };
 
